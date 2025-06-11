4f22c7d44003796851c93ee7f381c93863cf8a81
Adzan-Takhyan-Firdaus <adzan.takhyan.firdaus@gmail.com>
2025-06-11 21:37:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,7 @@ from wulan.queue_model import run_queue_model
 from tiyas.exponential_model import run_exponential_model
 
 st.set_page_config(page_title="Aplikasi Matematika Industri", layout="wide")
-st.sidebar.title("Kelompok 2 Matematika Terapan")
-st.sidebar.markdown("""
-- Adzan Takhyan Firdaus
-- Wulan Melinda Sari
-- Muhamad Rizky
-- Bila Rahmadani
-- Tiyas Sulis Setyawati
-""")
+
 
 st.sidebar.title("Aplikasi Matematika pada Lingkungan Industri")
 st.sidebar.write("Pilih salah satu model dari menu di bawah ini. Masukkan input yang diminta, lalu lihat hasil dan grafik.")
@@ -21,6 +14,15 @@ menu = st.sidebar.radio("Pilih Model", ["Optimasi Produksi (Linear Programming)"
                                         "Model Persediaan (EOQ)",
                                         "Model Antrian (M/M/1)",
                                         "Model Eksponensial"])
+
+st.sidebar.title("Kelompok 2 Matematika Terapan")
+st.sidebar.markdown("""
+- Adzan Takhyan Firdaus
+- Wulan Melinda Sari
+- Muhamad Rizky
+- Bila Rahmadani
+- Tiyas Sulis Setyawati
+""")
 
 if menu == "Optimasi Produksi (Linear Programming)":
     run_linear_programming()
